1caee93458d1189d288cf2f0549ece87803d6620
Diego <diegoteixeirapereira@hotmail.com>
2021-08-26 15:24:03 -0300

    criação do arquivo de configuração do dataSource

---
+++ src/main/java/br/com/java/springbootrestapi/config/DatasourceConfig.java
@@ -0,0 +1,20 @@
+package br.com.java.springbootrestapi.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DatasourceConfig {
+    @Bean
+    public DataSource datasource() {
+        return DataSourceBuilder.create()
+          .driverClassName("com.mysql.cj.jdbc.Driver")
+          .url("jdbc:mysql://localhost:3306/db_spring_boot_rest_vue")
+          .username("root")
+          .password("root")
+          .build();	
+    }
+}
